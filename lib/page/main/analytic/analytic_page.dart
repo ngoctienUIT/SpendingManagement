ede76473f08e57a723e7191cc6cbaca2470b3e98
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2022-09-16 08:56:45 +0700

    add tabbar page analytic
    thêm tabbar vào

--- lib/page/main/analytic/analytic_page.dart
+++ lib/page/main/analytic/analytic_page.dart
@@ -1,13 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:spending_management/constants/app_styles.dart';
 
-class AnalyticPage extends StatelessWidget {
+class AnalyticPage extends StatefulWidget {
   const AnalyticPage({Key? key}) : super(key: key);
+
+  @override
+  State<AnalyticPage> createState() => _AnalyticPageState();
+}
+
+class _AnalyticPageState extends State<AnalyticPage>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text("Analytic"),
-    ));
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Row(
+                children: const [
+                  Text(
+                    "Spending",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Container(
+                height: 40,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                    color: const Color.fromRGBO(242, 243, 247, 1),
+                    borderRadius: BorderRadius.circular(10)),
+                child: TabBar(
+                    controller: _tabController,
+                    labelColor: Colors.black87,
+                    labelStyle: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 16),
+                    unselectedLabelColor: const Color.fromRGBO(45, 216, 198, 1),
+                    unselectedLabelStyle: AppStyles.p,
+                    isScrollable: false,
+                    indicatorColor: Colors.red,
+                    indicator: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                    tabs: const [
+                      Tab(text: "Weekly"),
+                      Tab(text: "Monthly"),
+                      Tab(text: "Yearly")
+                    ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
