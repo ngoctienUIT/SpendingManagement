f9ee6e59341c807220435117782bcf85d2de0a83
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2022-09-18 18:27:44 +0700

    update logo and name
    cập nhật logo và tên app

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Spending Management',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
