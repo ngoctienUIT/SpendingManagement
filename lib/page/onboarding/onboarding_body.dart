2d8ad5f1dbc0a93ff530f72275a867b256225460
phongbj <20520364@gm.uit.edu.vn>
2022-09-21 09:58:08 +0700

    onboarding

---
+++ lib/page/onboarding/onboarding_body.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Onboardingbody extends StatelessWidget {
+   final String title;
+   final String description;
+   final String image;
+
+  const Onboardingbody({required this.title, required this.description, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(image, width: width * 0.6,),
+         const SizedBox(height: 30,),
+          Text(title, style:const TextStyle(
+            fontSize: 28, fontWeight: FontWeight.bold,
+          ),),
+          const SizedBox(height: 20,),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 80),
+          child: Text(description, style:const TextStyle(
+            height: 1.5,
+            fontSize: 14, fontWeight: FontWeight.normal, letterSpacing: 0.7
+          ), textAlign: TextAlign.center,)
+         ),
+          const SizedBox(height: 60,),
+        ]
+      )
+    );
+  }
+}
--- lib/page/onboarding/onboarding_page.dart
+++ lib/page/onboarding/onboarding_page.dart
@@ -1,19 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:spending_management/constants/function/on_will_pop.dart';
+import 'onboarding_body.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({Key? key}) : super(key: key);
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
 
+class _OnboardingPageState extends State<OnboardingPage> {
+  int currentPage = 0;
+  final PageController _pageController = PageController();
+
+  final List<Widget> _pages = [
+    const Onboardingbody(
+      title: "keep save",
+      description:
+      "accessToken",
+      image: "assets/images/anh11.jpg",
+    ),
+    const Onboardingbody(
+      title: "keep save",
+      description:
+      "accessToken",
+      image: "assets/images/anh1.jpg",
+    ),
+    const Onboardingbody(
+      title: "keep save",
+      description:
+      "accessToken",
+      image: "assets/images/anh22.png",
+    ),
+  ];
+  onPageChanged(int index){
+    setState(() {
+      currentPage = index;
+    });
+  }
+  DateTime? currentBackPressTime;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Skip"),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/login');
-          },
-        ),
-      ),
+        body: WillPopScope(
+          onWillPop: () => onWillPop(
+            action: (now) => currentBackPressTime = now,
+            currentBackPressTime: currentBackPressTime,
+          ),
+          child: Stack(
+            children: [
+              Expanded(
+                child: PageView.builder(
+                  scrollDirection: Axis.horizontal,
+                  controller: _pageController,
+                  itemCount: _pages.length,
+                  onPageChanged: (value) {
+                    setState(() {
+                      currentPage = value;
+                    });
+                  },
+                  itemBuilder: (BuildContext context, int index) {
+                    return _pages[index];
+                  },
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.center,
+                      children: List<Widget>.generate(_pages.length, (int index){
+                        return AnimatedContainer(duration:const Duration(microseconds: 300),
+                          height: 10,
+                          width: (index == currentPage)? 30:10,
+                          margin: const EdgeInsets.symmetric(horizontal: 5, vertical:30),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: (index == currentPage)
+                                ? Colors.blue
+                                : Colors.blue.withOpacity(0.5),
+                          ),
+                        );
+                      })),
+                  InkWell(
+                      onTap: (){
+                        _pageController.nextPage(duration: const Duration(milliseconds: 800), curve: Curves.easeInOutQuint);
+                        if (currentPage == 2 ){Navigator.pushReplacementNamed(context, "/login");}
+                      },
+                      child: AnimatedContainer(duration: const Duration(milliseconds: 300),
+                        height: 70,
+                        alignment: Alignment.center,
+                        width: (currentPage==(_pages.length-1))? 200:70,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(35),
+                        ),
+                        child:(currentPage==(_pages.length-1))? const Text("get stated", style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),): const Icon(Icons.navigate_next,color: Colors.white, size: 50,
+                        ),
+                      )
+                  ),
+                  const SizedBox(height: 50,
+                  ),
+                ],
+              )
+            ],
+          ),)
     );
   }
 }
