081900fd5325028b7f22d48129bb87b839228952
phongbj <20520364@gm.uit.edu.vn>
2022-12-07 11:39:37 +0700

    onbroading update

--- lib/page/onboarding/onboarding_body.dart
+++ lib/page/onboarding/onboarding_body.dart
@@ -1,22 +1,40 @@
-import 'package:flutter/material.dart';
 
-Widget itemOnBoarding(Map<String, String> item) {
-  return Container(
-    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-    child: Column(
-      children: [
-        const SizedBox(height: 10),
-        Text(
-          item["title"]!,
-          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: 20),
-        Text(
-          item["content"]!,
-          style: const TextStyle(fontSize: 16),
-        ),
-        // Image.asset(item["image"]!)
-      ],
-    ),
-  );
+import 'package:flutter/material.dart';
+import 'package:spending_management/setting/localization/app_localizations.dart';
+
+class ItemOnBoarding extends StatelessWidget {
+  const ItemOnBoarding({Key? key, required this.item}) : super(key: key);
+  final Map<String, String> item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(height: 10),
+          Text(
+            AppLocalizations.of(context).translate(item["title"]!),
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            AppLocalizations.of(context).translate(item["content"]!),
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontSize: 16),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            decoration:
+            BoxDecoration(border: Border.all(color: Colors.black54)),
+            child: Image.asset(
+              item["image"]!,
+              fit: BoxFit.fitHeight,
+              height: MediaQuery.of(context).size.height - 250,
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
--- lib/page/onboarding/onboarding_page.dart
+++ lib/page/onboarding/onboarding_page.dart
@@ -1,8 +1,11 @@
+
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'package:spending_management/page/onboarding/onboarding_body.dart';
+
 import '../../constants/app_colors.dart';
+import '../../constants/function/on_will_pop.dart';
 import '../../setting/localization/app_localizations.dart';
+import 'onboarding_body.dart';
 
 class OnBoardingPage extends StatefulWidget {
   const OnBoardingPage({Key? key}) : super(key: key);
@@ -14,21 +17,42 @@ class OnBoardingPage extends StatefulWidget {
 class _OnBoardingPageState extends State<OnBoardingPage> {
   final controller = PageController();
   bool isLastPage = false;
+  DateTime? currentBackPressTime;
   List<Map<String, String>> listPage = [
     {
-      "image": "assets/images/",
-      "title": "Trang chủ",
-      "content": "Xem tổng kết chi tiêu và số dư còn lại hàng tháng"
+      "image": "assets/intro/home.jpg",
+      "title": "home",
+      "content": "view_monthly_spending_summary_and_remaining_balance"
     },
     {
-      "image": "assets/images/",
-      "title": "Lịch",
-      "content": "Xem lại những chi tiêu theo lịch biểu"
+      "image": "assets/intro/calendar.jpg",
+      "title": "calendar",
+      "content": "review_your_scheduled_expenses"
     },
     {
-      "image": "assets/images/",
-      "title": "Báo cáo",
-      "content": "Xem báo cáo chi tiêu và thu nhập của bạn qua biểu đồ"
+      "image": "assets/intro/add.jpg",
+      "title": "add_spending",
+      "content": "add_and_edit_your_daily_spending"
+    },
+    {
+      "image": "assets/intro/share.jpg",
+      "title": "share",
+      "content": "share_your_spending_with_friends"
+    },
+    {
+      "image": "assets/intro/search.jpg",
+      "title": "search",
+      "content": "search_for_your_expenses"
+    },
+    {
+      "image": "assets/intro/analytic.jpg",
+      "title": "statistical",
+      "content": "view_your_spending_and_income_reports_through_graphs"
+    },
+    {
+      "image": "assets/intro/profile.jpg",
+      "title": "account",
+      "content": "edit_personal_information_and_other_settings"
     },
   ];
 
@@ -41,83 +65,91 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.only(bottom: 60),
-          child: PageView.builder(
-            controller: controller,
-            itemCount: listPage.length,
-            onPageChanged: (value) {
-              setState(() => isLastPage = value == 2);
-            },
-            itemBuilder: (context, index) => itemOnBoarding(listPage[index]),
+      body: WillPopScope(
+        onWillPop: () => onWillPop(
+          action: (now) => currentBackPressTime = now,
+          currentBackPressTime: currentBackPressTime,
+        ),
+        child: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.only(bottom: 60),
+            child: PageView.builder(
+              controller: controller,
+              itemCount: listPage.length,
+              onPageChanged: (value) {
+                setState(() => isLastPage = value == 6);
+              },
+              itemBuilder: (context, index) => ItemOnBoarding(
+                item: listPage[index],
+              ),
+            ),
           ),
         ),
       ),
       bottomSheet: isLastPage
           ? TextButton(
-              style: TextButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(0),
-                ),
-                foregroundColor: Colors.white,
-                backgroundColor: AppColors.buttonLogin,
-                minimumSize: const Size.fromHeight(60),
-              ),
-              onPressed: () {
-                Navigator.pushReplacementNamed(context, '/login');
-              },
-              child: Text(
-                AppLocalizations.of(context).translate('get_started'),
-                style: const TextStyle(fontSize: 20),
-              ),
-            )
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(0),
+          ),
+          foregroundColor: Colors.white,
+          backgroundColor: AppColors.buttonLogin,
+          minimumSize: const Size.fromHeight(60),
+        ),
+        onPressed: () {
+          Navigator.pushReplacementNamed(context, '/login');
+        },
+        child: Text(
+          AppLocalizations.of(context).translate('get_started'),
+          style: const TextStyle(fontSize: 20),
+        ),
+      )
           : Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              height: 60,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton(
-                    onPressed: () => controller.jumpToPage(2),
-                    child: Text(
-                      AppLocalizations.of(context).translate('skip'),
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                  ),
-                  Center(
-                    child: SmoothPageIndicator(
-                      controller: controller,
-                      count: 3,
-                      effect: WormEffect(
-                        spacing: 10,
-                        dotWidth: 10,
-                        dotHeight: 10,
-                        dotColor: Colors.black26,
-                        activeDotColor: Colors.teal.shade700,
-                      ),
-                      onDotClicked: (index) => controller.animateToPage(
-                        index,
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.easeIn,
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      controller.nextPage(
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.easeIn,
-                      );
-                    },
-                    child: Text(
-                      AppLocalizations.of(context).translate('next'),
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ],
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(
+              onPressed: () => controller.jumpToPage(6),
+              child: Text(
+                AppLocalizations.of(context).translate('skip'),
+                style: const TextStyle(fontSize: 16),
               ),
             ),
+            Center(
+              child: SmoothPageIndicator(
+                controller: controller,
+                count: 7,
+                effect: WormEffect(
+                  spacing: 10,
+                  dotWidth: 10,
+                  dotHeight: 10,
+                  dotColor: Colors.black26,
+                  activeDotColor: Colors.teal.shade700,
+                ),
+                onDotClicked: (index) => controller.animateToPage(
+                  index,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeIn,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                controller.nextPage(
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeIn,
+                );
+              },
+              child: Text(
+                AppLocalizations.of(context).translate('next'),
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
